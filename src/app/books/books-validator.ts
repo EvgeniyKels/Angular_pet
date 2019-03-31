1b48f6b5d985d2f0da83d2a204f2add003ce7675
zenitfz@gmail.com <8SkKQxZr>
2019-03-31 16:10:57 +0400

    25_03_2018

--- src/app/books/books-validator.ts
+++ src/app/books/books-validator.ts
@@ -1,7 +1,13 @@
 import {AbstractControl, ValidationErrors} from '@angular/forms';
+import {Promise} from 'q';
+import {BooksService} from './books.service';
 
 export class BooksValidators {
+  static booksService: BooksService;
   static uniqueId(control: AbstractControl): Promise<ValidationErrors|null> {
-    return
-  };
+    const id = control.value;
+    return new Promise(resolve => {
+      BooksValidators.booksService.getBook(id).subscribe(() => resolve({unique: true}), () => resolve(null));
+    });
+  }
 }
